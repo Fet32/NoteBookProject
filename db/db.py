0f34bc8eb52f4d4c153c08de13622b33bd0be28b
fet32 <fmanaseev@gmail.com>
2023-03-12 23:49:08 +0300

    db test time on feel data

--- db/db.py
+++ db/db.py
@@ -38,23 +38,27 @@ class MySQL(BaseDB):
 				password='testpassword',
 				host='localhost',
 				port=3307,
-				database='notebook'
+				database='notebook',
+				# auth_plugin='mysql_native_password'
 			)
 		except Exception as exc:
 			print(exc)
 
-	def create_cursor(self):
-		return self.connection.cursor(buffered=True)
+	def create_cursor(self) -> connector.connection.MySQLCursor:
+		return self.connection.cursor()#buffered=True)
 
 	def close(self):
 		self.connection.close()
 
 	def execute(self, query: str) -> tuple:
 
-		with self.connection.cursor() as cur:
+		with self.create_cursor() as cur: #self.connection.cursor() as cur:
 			cur.execute(query)
 
 			return tuple(cur)
+
+	def commit(self):
+		self.connection.commit()
 
 
 class Notebook(object):
@@ -74,15 +78,30 @@ db_test = MySQL()
 nb = Notebook(db=db_test)
 nb.connect_db()
 
-num_req = 20000
+num_req = 100
 
 ##############################################
 
+# db_test.execute(query='insert into user (name) values ("Fet")')
+# db_test.execute(query='insert into book (name,user_id) values ("book1", 12)')
+# for i in range(2,10):
+# 	db_test.execute(query='insert into book_field (field_name, field_type, book_id) values ("field%s","txt",1)'%i)
+# for record in range(2000):
+# 	for i in range(1,10):
+# 		db_test.execute(query='''
+# 							insert into book_record
+# 							(record_id, field_id, value_num, value_txt)
+# 							values
+# 							(%(record)d, %(i)d, %(i)d, "field%(i)s")
+# 							''' % {'record': record, 'i': i})
+#
+# db_test.commit()
+
 start_a = time.perf_counter()
-all_types = db_test.execute(query='SELECT * FROM field_type')
+all_types = db_test.execute(query='SELECT * FROM book_field')
 
 for i in range(num_req):
-	r = db_test.execute(query='SELECT * FROM book_field WHERE field_name="field1"')
+	r = db_test.execute(query='SELECT * FROM book_record WHERE value_txt="field5"')
 
 fin_a = time.perf_counter() - start_a
 print('---> only select', fin_a)
@@ -94,10 +113,10 @@ start_b = time.perf_counter()
 for i in range(num_req):
 
 	QUERY_SELECT_JOIN = """
-	SELECT book_field.id, book_field.field_name, book_field.book_id, field_type.name
-	FROM book_field
-	INNER JOIN field_type
-	ON book_field.type_id=field_type.id and field_name="field1";
+	SELECT book_record.id, book_record.record_id, book_record.field_id, book_field.field_name
+	FROM book_record
+	INNER JOIN book_field
+	ON book_record.field_id=book_field.id and field_name="field5";
 	"""
 
 	db_test.execute(query=QUERY_SELECT_JOIN)
